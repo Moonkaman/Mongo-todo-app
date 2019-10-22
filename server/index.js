dfbf48f89798fa134d11e94b7df7c5d518b7fac7
Moonkaman <nickbatmanglidj@gmail.com>
2019-10-22 13:26:26 -0600

    Implement full CRUD functionality for todos

--- server/index.js
+++ server/index.js
@@ -23,6 +23,39 @@ server.get('/todos', (req, res) => {
   })
 })
 
+server.put('/todos/:id', (req, res) => {
+  const todoID = db.getPrimaryKey(todoID);
+
+  db.getDB().collection(collection).findOneAndUpdate({_id: todoID}, {$set: {todo: req.body.todo}}, {returnOriginal: false}, (err, result) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.status(200).json(result);
+    }
+  })
+})
+
+server.delete('/todos/:id', (req, res) => {
+  const todoID = db.getPrimaryKey(req.params.id);
+  db.getDB().collection(collection).findOneAndDelete({_id: todoID}, (err, result) => {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
+
+server.post('/todos', (req, res) => {
+  db.getDB().collection(collection).insertOne(req.body, (err, result) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.status(201).json(result);
+    }
+  })
+})
+
 db.connect(err => {
   if(err) {
     console.log('unable to connect to database');
